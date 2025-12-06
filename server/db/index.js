e440b3b1aed93496eb118f204047312d79037332
Matthew Daos <143854012+matthewdaos@users.noreply.github.com>
2025-12-06 18:11:36 -0500

    Used Mongo for main database manager

--- server/db/index.js
+++ server/db/index.js
@@ -1,15 +1,4 @@
+const dbManager = require('./mongo/index');
 
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-dotenv.config();
-
-mongoose
-    .connect(process.env.DB_CONNECT, { useNewUrlParser: true })
-    .catch(e => {
-        console.error('Connection error', e.message)
-    })
-
-const db = mongoose.connection
-
-module.exports = db
+module.exports = dbManager;
 
