a171f30ff454e1e0f5db7f01e5cc7ff630f6b016
Matthew Daos <143854012+matthewdaos@users.noreply.github.com>
2025-12-06 16:02:35 -0500

    Made getPlaylistById acccessible if logged in, rather than as a guest

--- server/routes/store-router.js
+++ server/routes/store-router.js
@@ -5,11 +5,12 @@ const auth = require('../auth')
 
 router.post('/playlist', auth.verify, StoreController.createPlaylist)
 router.delete('/playlist/:id', auth.verify, StoreController.deletePlaylist)
-router.get('/playlist/:id', auth.guestVerify, StoreController.getPlaylistById)
+router.get('/playlist/:id', auth.verify, StoreController.getPlaylistById)
 router.get('/playlistpairs', auth.verify, StoreController.getPlaylistPairs)
-router.get('/playlists', auth.guestVerify, StoreController.searchPlaylists)
 router.put('/playlist/:id', auth.verify, StoreController.updatePlaylist)
 router.post('/playlist/:id/copy', auth.verify, StoreController.copyPlaylist)
+
+router.get('/playlists', auth.guestVerify, StoreController.searchPlaylists)
 router.post('/playlist/:id/play', auth.guestVerify, StoreController.playPlaylist)
 
 module.exports = router
