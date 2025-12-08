8639c7883c7f15a5bca8abed031ab34c399a92d4
Matthew Daos <143854012+matthewdaos@users.noreply.github.com>
2025-12-08 15:40:11 -0500

    Renamed song router route

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use('/auth', authRouter)
 const storeRouter = require('./routes/store-router')
 app.use('/store', storeRouter)
 const songRouter = require('./routes/song-router')
-app.use('/song', songRouter)
+app.use('/store/song', songRouter)
 
 // INITIALIZE OUR DATABASE OBJECT
 const db = require('./db')
