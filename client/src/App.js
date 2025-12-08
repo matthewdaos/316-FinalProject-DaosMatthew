0cf060bc92cb902d41ba822ed4a9129f62ec4029
Matthew Daos <143854012+matthewdaos@users.noreply.github.com>
2025-12-07 21:10:58 -0500

    Layed out first steps with user verification page: welcome, login, creating account, continuing as guest

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,27 @@
-import './App.css';
-import { React } from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { AuthContextProvider } from './auth';
-import { GlobalStoreContextProvider } from './store'
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import {
     AppBanner,
-    HomeWrapper,
+    HomeScreen,
     LoginScreen,
     RegisterScreen,
-    Statusbar,
-    WorkspaceScreen
-} from './components'
-/*
-  This is the entry-point for our application. Notice that we
-  inject our store into all the components in our application.
-  
-  @author McKilla Gorilla
-*/
-const App = () => {   
+    EditAccountScreen,
+    PlaylistScreen,
+} from './components';
+
+function App () {   
     return (
         <BrowserRouter>
-            <AuthContextProvider>
-                <GlobalStoreContextProvider>              
-                    <AppBanner />
-                    <Switch>
-                        <Route path="/" exact component={HomeWrapper} />
-                        <Route path="/login/" exact component={LoginScreen} />
-                        <Route path="/register/" exact component={RegisterScreen} />
-                        <Route path="/playlist/:id" exact component={WorkspaceScreen} />
-                    </Switch>
-                    <Statusbar />
-                </GlobalStoreContextProvider>
-            </AuthContextProvider>
+            <AppBanner />
+            <Switch>
+                <Route path="/" exact component={HomeScreen} />
+                <Route path="/login" component={LoginScreen} />
+                <Route path="/create-account" component={RegisterScreen} />
+                <Route path="/edit-account" component={EditAccountScreen} />
+                <Route path="/playlists" component={PlaylistScreen} />
+            </Switch>
         </BrowserRouter>
-    )
+    );
 }
 
 export default App
