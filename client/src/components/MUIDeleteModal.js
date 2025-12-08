5dfee2683c6403ca65cf70026e89e2326cfee6a3
Matthew Daos <143854012+matthewdaos@users.noreply.github.com>
2025-12-08 12:18:59 -0500

    Fixed the delete playlist modal to match the one shown in the specification document

--- client/src/components/MUIDeleteModal.js
+++ client/src/components/MUIDeleteModal.js
@@ -1,58 +1,67 @@
-import { useContext } from 'react'
-import GlobalStoreContext from '../store';
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
-
-const style1 = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 345,
-    height: 250,
-    backgroundSize: "contain",
-    backgroundImage: `url(https://i.insider.com/602ee9ced3ad27001837f2ac?})`,
-    border: '3px solid #000',
-    padding: '20px',
-    boxShadow: 24,
-};
+import { useContext } from "react";
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography, Box } from "@mui/material";
+import { GlobalStoreContext } from "../store";
 
 export default function MUIDeleteModal() {
     const { store } = useContext(GlobalStoreContext);
-    let name = "";
-    if (store.listMarkedForDeletion) {
-        name = store.listMarkedForDeletion.name;
-    }
-    function handleDeleteList(event) {
+
+    const open = store.isDeleteListModalOpen();
+    const playlist = store.listMarkedForDeletion;
+
+    const name = playlist ? playlist.name : "";
+
+    function handleConfirm() {
         store.deleteMarkedList();
     }
-    function handleCloseModal(event) {
+
+    function handleCancel() {
         store.hideModals();
     }
 
     return (
-        <Modal
-        open={store.listMarkedForDeletion !== null}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        <Dialog
+            open={open}
+            onClose={handleCancel}
+            maxWidth="xs"
+            fullWidth
+            PaperProps={{
+                sx: {
+                    backgroundColor: "#b6ffb6", // light green like your screenshot
+                },
+            }}
         >
-        <Box sx={style1}>
-            <Typography sx={{fontWeight: 'bold'}} id="modal-modal-title" variant="h4" component="h2">
-                Delete Playlist
-            </Typography>
-            <Divider sx={{borderBottomWidth: 5, p: '5px', transform: 'translate(-5.5%, 0%)', width:377}}/>
-            <Box sx={{background: "rgb(172,79,198,0.05)"}}>
-            <Typography id="modal-modal-description" variant="h6" sx={{color: "#301974" ,fontWeight: 'bold', mt: 1}}>
-                Are you sure you want to delete the <Typography display="inline" id="modal-modal-description" variant="h6" sx={{color: "#820747CF" ,fontWeight: 'bold', mt: 2, textDecoration: 'underline'}}>{name}</Typography> playlist?
-            </Typography>
-            </Box>
-            <Button sx={{opacity: 0.7, color: "#8932CC", backgroundColor: "#CBC3E3", fontSize: 13, fontWeight: 'bold', border: 2, p:"5px", mt:"60px", mr:"95px"}} variant="outlined" onClick={handleDeleteList}> Confirm </Button>
-            <Button sx={{opacity: 0.50, color: "#8932CC", backgroundColor: "#CBC3E3", fontSize: 13, fontWeight: 'bold', border: 2, p:"5px", mt:"60px", ml:"102px"}} variant="outlined" onClick={handleCloseModal}> Cancel </Button>
-        </Box>
-    </Modal>
+            <DialogTitle sx={{ fontWeight: "bold", backgroundColor: "#008000", color: "white" }}>
+                Delete playlist?
+            </DialogTitle>
+
+            <DialogContent sx={{ pt: 3 }}>
+                <Typography variant="h6" sx={{ mb: 2 }}>
+                    Are you sure you want to delete the {name} playlist?
+                </Typography>
+                <Typography variant="body2">
+                    Doing so means it will be permanently removed.
+                </Typography>
+            </DialogContent>
+
+            <DialogActions>
+                {/* Wrap buttons in a Box so they align to the far left */}
+                <Box sx={{ display: "flex", justifyContent: "flex-start", width: "100%", pl: 2, pb: 2 }}>
+                    <Button
+                        variant="contained"
+                        color="error"
+                        sx={{ mr: 2 }}
+                        onClick={handleConfirm}
+                    >
+                        Delete Playlist
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        onClick={handleCancel}
+                    >
+                        Cancel
+                    </Button>
+                </Box>
+            </DialogActions>
+        </Dialog>
     );
 }
