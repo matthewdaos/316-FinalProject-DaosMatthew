32270d374f8601aadf9f7d3e8469bb697f253c3d
Matthew Daos <143854012+matthewdaos@users.noreply.github.com>
2025-12-08 15:50:58 -0500

    Added the play playlist modal to the index file

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -6,10 +6,12 @@ import LoginScreen from './LoginScreen'
 import MUIDeleteModal from './MUIDeleteModal'
 import MUIEditPlaylistModal from './MUIEditPlaylistModal'
 import MUIEditSongModal from './MUIEditSongModal'
+import MUIPlayPlaylistModal from './MUIPlayPlaylistModal'
 import MUIErrorModal from './MUIErrorModal'
 import PlaylistScreen from './PlaylistScreen'
 import RegisterScreen from './RegisterScreen'
 import SongCard from './SongCard'
+import SongScreen from './SongScreen'
 import SplashScreen from './SplashScreen'
 import Statusbar from './Statusbar'
 import WorkspaceScreen from './WorkspaceScreen'
@@ -23,9 +25,11 @@ export {
     MUIDeleteModal,
     MUIEditPlaylistModal,
     MUIEditSongModal,
+    MUIPlayPlaylistModal,
     MUIErrorModal,
     PlaylistScreen,
     SongCard,
+    SongScreen,
     RegisterScreen,
     SplashScreen,
     Statusbar, 
